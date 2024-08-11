ed49e18669964b259d80c5cd5db4b14ed9c0a58a
Boranbektas <boranbek1234@gmail.com>
2024-08-11 17:35:33 +0300

    Implemented a post method,created prediction_result.html file

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from .views import home
 
+
 def create_app():
-    app = Flask(__name__,template_folder="templates",static_folder="static")
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     #app.config.from_pyfile("config.py")
     app.register_blueprint(home.bp)
 
--- app/views/home.py
+++ app/views/home.py
@@ -1,8 +1,12 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 bp = Blueprint("home", __name__, template_folder="../templates")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def Index():
+    if request.method == "POST":
+        if request.form:
+
+
     return render_template("index.html")
